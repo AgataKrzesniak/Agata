01bec880585600fd8665e8d5027bd26803b027fa
Agata Krzesniak <agatakrzesniak999@gmail.com>
2017-02-05 16:06:28 +0000

    add funkcje

---
+++ 170206/funkcje.java
@@ -0,0 +1,23 @@
+public class funkcje {
+    public void pierwsza(int a ) {
+    boolean pierwsza = true;
+
+ for(int i=2;i*i<=a;i++)
+        if (a%i==0)
+         pierwsza = false;
+    if(pierwsza)
+   System.out.println("liczba pierwsza");
+  else
+   System.out.println("liczba złożona");
+
+
+    }
+    public static void main (String[] args){
+   funkcje funk=new funkcje();
+    int a= Integer.parseInt(args[0]);
+    funk.pierwsza(a);
+       
+            
+        
+    }
+}
