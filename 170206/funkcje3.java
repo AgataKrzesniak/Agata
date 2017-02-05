eacda7efa3a3fddfe6a840c28c6d6a82ca12bdc5
Agata Krzesniak <agatakrzesniak999@gmail.com>
2017-02-05 16:12:27 +0000

    add funkcje3

---
+++ 170206/funkcje3.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Arrays;
+public class funkcje3{
+    int [] los(){
+        int[] t= new int[10];
+        Random rand=new Random();
+        for(int i=0; i<10 ; i++)
+        { t[i]=rand.nextInt(101)+0;}
+        return t;
+    } // wiem że program źle działa ale już tylę się z nim męczyłam i na nic nie mogę wpaść 
+      void pierwsza(int[] t ) {
+          boolean pierwsza=true;
+for(int i=0; i<t.length; i++){
+    for (int j=2; j*j<=t.length; j++)
+    if(t[i]%j==0)
+    pierwsza=false;
+    else
+    pierwsza = true;
+    if(pierwsza)
+   System.out.println(t[i] +" liczba pierwsza");
+  else
+   System.out.println(t[i] +" liczba złożona");
+}
+      }
+    public static void main (String[] args){
+   funkcje3 funk=new funkcje3();
+       funk.pierwsza(funk.los());
+
+            
+    }
+}
