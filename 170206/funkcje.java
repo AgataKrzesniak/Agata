88b0d1863b9b466e7976ec331c441afd61cc21b8
Agata Krzesniak <agatakrzesniak999@gmail.com>
2017-02-07 18:09:14 +0000

    add funkcje

--- 170206/funkcje.java
+++ 170206/funkcje.java
@@ -2,7 +2,7 @@ public class funkcje {
     public void pierwsza(int a ) {
     boolean pierwsza = true;
 
- for(int i=2;i*i<=a;i++)
+ for(int i=2;i*i<a;i++)
         if (a%i==0)
          pierwsza = false;
     if(pierwsza)
